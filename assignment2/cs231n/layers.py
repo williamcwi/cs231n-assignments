2126314fd69c29f4b1130fde6959fa1a065dc944
williamcwi <williamcwi.chu@hotmail.com>
2023-10-27 15:21:52 +0100

    completed batchnorm forward

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -182,7 +182,21 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # Referencing the original paper (https://arxiv.org/abs/1502.03167)   #
         # might prove to be helpful.                                          #
         #######################################################################
-        pass
+        sample_mean = np.mean(x, axis=0)
+        sample_var = np.var(x, axis=0)
+        x_hat = (x - sample_mean) / np.sqrt(sample_var + eps)
+        out = gamma * x_hat + beta
+
+        cache = {'x': x,
+                 'mean': sample_mean,
+                 'var': sample_var,
+                 'x_hat': x_hat,
+                 'gamma': gamma,
+                 'beta': beta
+                 }
+
+        running_mean = momentum * running_mean + (1 - momentum) * sample_mean
+        running_var = momentum * running_var + (1 - momentum) * sample_var
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -193,7 +207,8 @@ def batchnorm_forward(x, gamma, beta, bn_param):
         # then scale and shift the normalized data using gamma and beta.      #
         # Store the result in the out variable.                               #
         #######################################################################
-        pass
+        running_x_hat = (x - running_mean) / np.sqrt(running_var + eps)
+        out = gamma * running_x_hat + beta
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################
